590d0d500ae5d6397d1d63e48e95c6729b5dbb48
unknown <unknown>
2025-06-11 13:32:15 +0700

    update

--- constants/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DAPP_NAME = "DOT_HACK_CAMP";
-
-export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
---
+++ src/components/AppShell/AppShell.tsx
@@ -0,0 +1,177 @@
+import {
+  ActionIcon,
+  Anchor,
+  AppShell as MantineAppShell,
+  Button,
+  Group,
+  Image,
+  Paper,
+  Stack,
+  useComputedColorScheme,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+  IconBoxSeam,
+  IconBrandGithubFilled,
+  IconBrandXFilled,
+  IconBrightnessDown,
+  IconChartDots3,
+  IconExternalLink,
+  IconMoon,
+  IconNotes,
+  IconRoute,
+  IconWorldWww,
+  IconZoomCode,
+} from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import { NAVIGATION_ITEMS } from '../../constants';
+import { useWallet } from '../../hooks';
+import { XcmRouterPage } from '../../routes';
+import { LinksGroup } from '../LinksGroup/LinksGroup';
+import { PageRoute } from '../PageRoute';
+import { Header } from './Header/Header';
+import classes from './Navbar.module.css';
+
+export const AppShell = () => {
+  const [opened, { toggle }] = useDisclosure();
+
+  const {
+    connectWallet,
+    changeAccount,
+    handleApiSwitch,
+    apiType,
+    isLoadingExtensions,
+    isInitialized,
+  } = useWallet();
+
+  const onMobileMenuClick = () => {
+    toggle();
+  };
+
+  const { setColorScheme } = useMantineColorScheme();
+
+  const computedColorScheme = useComputedColorScheme('light');
+
+  const toggleColorScheme = () => {
+    setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark');
+  };
+
+  const [pinned, setPinned] = useState(true);
+
+  const onChangeAccountClick = () => void changeAccount();
+
+  const onConnectWalletClick = () => void connectWallet();
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 20) {
+        setPinned(false);
+      } else {
+        setPinned(true);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (
+      location.pathname === PageRoute.XCM_ROUTER.toString() &&
+      apiType === 'PJS'
+    ) {
+      handleApiSwitch('PAPI');
+    }
+  }, [location.pathname, apiType]);
+
+  const theme = useMantineTheme();
+
+  const colorScheme = useComputedColorScheme();
+
+  return (
+    <MantineAppShell
+      header={{ height: pinned ? 100 : 64, offset: false }}
+      navbar={{
+        width: 280,
+        breakpoint: 'xs',
+        collapsed: { mobile: !opened },
+      }}
+      padding={{
+        base: 16,
+        xs: 24,
+      }}
+      style={{
+        paddingTop: 48,
+        paddingLeft: 0,
+        paddingRight: 0,
+      }}
+      layout="alt"
+    >
+      <MantineAppShell.Header
+        withBorder={false}
+        px="xl"
+        style={{
+          transition:
+            'height 100ms ease, box-shadow 100ms ease, background-color 100ms ease, backdrop-filter 100ms ease',
+          boxShadow: pinned ? 'none' : theme.shadows.xs,
+          zIndex: 105,
+          clipPath: 'inset(0 -15px -150px 0)',
+        }}
+        bg={
+          pinned ? 'transparent' : colorScheme === 'light' ? 'white' : 'dark.7'
+        }
+      >
+        <Header
+          onMenuClick={onMobileMenuClick}
+          menuOpened={opened}
+          apiTypeInitialized={isInitialized}
+          isLoadingExtensions={isLoadingExtensions}
+          isPinned={pinned}
+          onApiTypeChange={handleApiSwitch}
+          onChangeAccountClick={onChangeAccountClick}
+          onConnectWalletClick={onConnectWalletClick}
+        />
+      </MantineAppShell.Header>
+      <MantineAppShell.Navbar
+        withBorder={false}
+        style={{
+          paddingTop: opened ? 100 : 0,
+        }}
+      >
+        <Paper shadow="sm" radius={0} className={classes.navbar}>
+          <Group className={classes.header} pt="0" pb="lg">
+            <Group w={160} flex={1}>
+              <Anchor href="https://paraspell.xyz/" target="_blank">
+                <Image src="/og-logo.png" fit="contain" px={8} pl={16} />
+              </Anchor>
+            </Group>
+            <ActionIcon variant="light" size="md" onClick={toggleColorScheme}>
+              {computedColorScheme === 'dark' ? (
+                <IconBrightnessDown size={22} />
+              ) : (
+                <IconMoon size={14} />
+              )}
+            </ActionIcon>
+          </Group>
+
+          <LinksGroup
+            label="XCM Router"
+            icon={IconRoute}
+            url={PageRoute.XCM_ROUTER}
+          />
+        </Paper>
+      </MantineAppShell.Navbar>
+      <MantineAppShell.Main className={classes.appShellMain}>
+        <Routes>
+          <Route path={PageRoute.XCM_ROUTER} Component={XcmRouterPage} />
+        </Routes>
+      </MantineAppShell.Main>
+    </MantineAppShell>
+  );
+};
